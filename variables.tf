4da27167e4f7e85302c35ae15b2548b10059ca58
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-22 11:00:19 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {}
+variable "gcp_project" {}
+variable "credentials" {}
+variable "sonarvpc" {}
+variable "sonarsub" {}
+// variable "elk_instance_name" {}
+variable "instance_name" {}
+variable "sonarqube_instance_ip_name" {}
+variable "sonarqube_instance_ip_region" {}
