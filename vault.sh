3c1f3ff84453e4ea5c7d5f7928f21a3b494d6932
Diksha Bajaj <47319191+Diksha86@users.noreply.github.com>
2019-10-23 15:55:21 +0530

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -9,7 +9,8 @@ sudo mkdir -p /logs/vault/
 gsutil mb gs://vaultbucky00
 sudo echo 'export IP=`curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`' >> ~/.bash_profile
 source ~/.bash_profile
-sudo touch /etc/vault/config.json 
+sudo touch /etc/vault/config.json
+sudo chmod 777 config.json
 sudo cat > /etc/vault/config.json  << EOF
 {
 "listener": [{
@@ -31,7 +32,8 @@ sudo cat > /etc/vault/config.json  << EOF
 }
 EOF
 
-sudo touch /etc/systemd/system/vault.service 
+sudo touch /etc/systemd/system/vault.service
+sudo chmod 777 vault.service
 sudo cat > /etc/systemd/system/vault.service  << EOF
 [Unit]
 Description=vault service
@@ -60,6 +62,3 @@ sudo su
 cd ~
 export VAULT_ADDR=http://'$IP':8200
 echo "export VAULT_ADDR=http://'$IP':8200" >> ~/.bashrc
-sudo vault status
-sudo vault operator init > /etc/vault/init.file
-sudo cat /etc/vault/init.file
