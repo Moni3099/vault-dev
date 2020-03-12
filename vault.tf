270d6d8122c2a6449461c3ccb5569f6a89c38d3d
Monica S <54371902+Moni3099@users.noreply.github.com>
2020-03-12 16:28:54 +0530

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("${var.credentials}")}"
+  //credentials = "${file("${var.credentials}")}"
   project     = "${var.gcp_project}"
   region      = "${var.region}"
 }
